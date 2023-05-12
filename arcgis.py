0a103ef9c0721ec3e0c24393d642fbf735019390
hufengyi11 <hufengyi_work@icloud.com>
2023-05-12 14:06:41 +0100

    epn polygon output

--- arcgis.py
+++ arcgis.py
@@ -1,3 +1,18 @@
+# Name: CreateThiessenPolygons_Example2.py
+# Description: Creates Thiessen polygons
+# https://pro.arcgis.com/en/pro-app/latest/tool-reference/analysis/create-thiessen-polygons.html
+ 
+# Import system modules
 import arcpy
+ 
+# Set environment settings
 arcpy.env.workspace = "C:/data/data.gdb"
-arcpy.analysis.CreateThiessenPolygons("schools", "c:/output/output.gdb/thiessen1", "ALL")
+ 
+# Set local variables
+# The point input features from which Thiessen polygons will be generated. As feature layer
+inFeatures = "schools"
+outFeatureClass = "c:/output/output.gdb/thiessen1"
+outFields = "ALL"
+ 
+# Run CreateThiessenPolygons
+arcpy.analysis.CreateThiessenPolygons(inFeatures, outFeatureClass, outFields)
