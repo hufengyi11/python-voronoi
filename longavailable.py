c1d02a03875b20c66ca2d02863b67ca700071b65
hufengyi11 <hufengyi_work@icloud.com>
2023-05-12 14:09:01 +0100

    clean data

--- longavailable.py
+++ longavailable.py
@@ -6,4 +6,4 @@ from longsgis import voronoiDiagram4plg
 builtup = gpd.read_file('data/EPN/epn_bsp.geojson'); builtup.crs = 32650
 boundary = gpd.read_file('data/EPN/epn_ops_boundaries.geojson'); boundary.crs = 32650
 vd = voronoiDiagram4plg(builtup, boundary)
-vd.to_file('epn_output.geojson', driver='GeoJSON')
+vd.to_file('data/EPN/epn_polygon_output.geojson', driver='GeoJSON')
