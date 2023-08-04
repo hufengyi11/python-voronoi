6712322862c88766e7b593c27ad41e85c3141873
hufengyi11 <hufengyi_work@icloud.com>
2023-08-04 14:05:06 +0100

    added npg data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 from longsgis import voronoiDiagram4plg
 
-builtup = gpd.read_file('data/EPN/epn_bsp.geojson'); builtup.crs = 32650
-boundary = gpd.read_file('data/EPN/epn_ops_boundaries.geojson'); boundary.crs = 32650
+builtup = gpd.read_file('data/npg-northeast/substation_sites_list.geojson'); builtup.crs = 32650
+boundary = gpd.read_file('data/npg-northeast/northern-powergrid-areas.geojson'); boundary.crs = 32650
 vd = voronoiDiagram4plg(builtup, boundary)
-vd.to_file('data/EPN/epn_polygon_output.geojson', driver='GeoJSON')
+vd.to_file('data/npg-northeast/npg-northeast-polygon.geojson', driver='GeoJSON')
