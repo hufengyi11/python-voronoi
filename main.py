b81062beeda8e1425d3084523e2e43185ce230c9
hufengyi11 <hufengyi_work@icloud.com>
2023-08-04 16:19:25 +0100

    npg-northeast voronoi polygon crreated

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import geopandas as gpd
 from longsgis import voronoiDiagram4plg
 
-builtup = gpd.read_file('data/npg-northeast/substation_sites_list.geojson'); builtup.crs = 32650
-boundary = gpd.read_file('data/npg-northeast/northern-powergrid-areas.geojson'); boundary.crs = 32650
+builtup = gpd.read_file('data/npg-northeast/substation_sites_list.geojson'); 
+# builtup.crs = 32650
+boundary = gpd.read_file('data/npg-northeast/northern-powergrid-areas.geojson'); 
+# boundary.crs = 32650
 vd = voronoiDiagram4plg(builtup, boundary)
-vd.to_file('data/npg-northeast/npg-northeast-polygon.geojson', driver='GeoJSON')
+vd.to_file('data/npg-yorkshire/npg-yorkshire-polygon.geojson', driver='GeoJSON')
